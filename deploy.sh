9d6bea913e441fa0ee11d948208b6f5bbd3843cd
SainyTK <tanakorn0412@gmail.com>
2023-10-22 22:21:46 +0700

    Add more yieldVault

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 source .env
-export RPC_URL=$FILECOIN_CALIBRATION_RPC_URL
-export EXPLORER_URL=$FILECOIN_CALIBRATION_SCAN_URL
-export EXPLORER_API_KEY=$FILECOIN_CALIBRATION_SCAN_API_KEY
+export RPC_URL=$GOERLI_RPC_URL
+export EXPLORER_URL=$GOERLI_SCAN_URL
+export EXPLORER_API_KEY=$GOERLI_SCAN_API_KEY
 
 # forge script script/deploy/DeployAll.s.sol --rpc-url $RPC_URL --verifier-url $EXPLORER_URL --etherscan-api-key $EXPLORER_API_KEY --broadcast --verify
-forge script script/deploy/DeployTestERC20.s.sol --rpc-url $RPC_URL  --broadcast
+# forge script script/deploy/DeployTestERC20.s.sol --rpc-url $RPC_URL --verifier-url $EXPLORER_URL --etherscan-api-key $EXPLORER_API_KEY --broadcast --verify
+forge script script/deploy/DeployTestERC4626.s.sol --rpc-url $RPC_URL --verifier-url $EXPLORER_URL --etherscan-api-key $EXPLORER_API_KEY --broadcast --verify
